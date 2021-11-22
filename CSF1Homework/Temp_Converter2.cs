7e92a2775a59821fbfb11c1b99b522844922c2a2
Don Richardson <donrichardson092388@outlook.com>
2021-11-22 12:09:01 -0600

    created new branch for temp converter 2 homerwork

---
+++ CSF1Homework/Temp_Converter2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Temp_Converter2
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
