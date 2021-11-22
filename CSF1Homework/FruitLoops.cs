2b50cb01603d4b617fe3b4ba9e71125bd15d966c
Don Richardson <donrichardson092388@outlook.com>
2021-11-22 10:45:57 -0600

    completed fruit loops sections

---
+++ CSF1Homework/FruitLoops.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoops
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Fruit Loops Pt 1.");
+            string[] fruit = { "apple", "banana", "kiwi", "pineapple", "lemon", "lime" };
+
+            for (int i = 0; i<fruit.Length; i++)
+            {
+                Console.WriteLine(fruit[i]);
+            }
+
+        }//end svm
+    }//end class
+}//end namespace
---
+++ CSF1Homework/Fruit_Loops2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Fruit_Loops2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fruit Loops Pt 2");
+            string[] fruit = { "apple", "banana", "kiwi", "pineapple", "lemon", "lime" };
+            string dots = "";
+            for (int i = 0; i < fruit.Length; i++)
+            {
+                if (i != 0)
+                {
+                    dots += ".";
+                }
+
+                Console.WriteLine(fruit[i]);
+                Console.WriteLine(dots);
+            }
+        }
+    }
+}
